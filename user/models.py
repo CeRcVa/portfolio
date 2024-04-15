4e5e794387c1df11171f14ee97572ee52fbad633
CeRcVa <shotikogamer16@gmail.com>
2024-04-15 19:59:39 +0400

    dasawyisi

---
+++ user/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # Provide unique related_name for groups and user_permissions
+    groups = models.ManyToManyField(Group, related_name='custom_user_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set', blank=True)
+
+    def __str__(self):
+        return self.email
---
+++ user/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from .views import (
+    RegisterView,
+    LoginView,
+    LogoutView,
+    ResetPasswordView,
+    ChangePasswordView,
+    CurrentUserView,
+    GetUserByIdView,
+    MyTokenObtainPairView
+)
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+    path('current-user/', CurrentUserView.as_view(), name='current-user'),
+    path('get-user/<int:user_id>/', GetUserByIdView.as_view(), name='get-user-by-id'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
